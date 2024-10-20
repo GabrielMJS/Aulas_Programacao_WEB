bbc90846a9e030fecf102d3b119c0ed67e7bd8e3
Gabriel Madureira <132027963+GabrielMJS@users.noreply.github.com>
2024-10-20 14:46:20 -0300

    exercicos e atividades para entregar

---
+++ 10_18/Pessoa.cs
@@ -0,0 +1,43 @@
+﻿using System.Reflection.Metadata;
+using System.Security.Cryptography;
+
+namespace ExemploWebApiPoo.Dominio
+{
+    public class Pessoa
+    {
+        public string cpf;
+        public string nome;
+        public DateTime dataNascimento;
+        public string nomeMae;
+        public string telefone;
+        public string email;
+        //Métodos CRUD
+        public void Inserir()
+        { 
+            //Incluir Pessoa;
+        }public void Alterar(Int32 id)
+        {
+            //Alterar Pessoa;
+        }public void Excluir(Int32 id)
+        {
+            //Excluir Pessoa;
+        }
+        public string PesquisarTodos() 
+        {
+            string retorno = $"CPF: {cpf}\r\n " +
+                $"Nome: {nome}" +
+                $"Nome da Mãe: {nomeMae}" +
+                $"Telefone: {telefone}";
+            return retorno;
+        }
+        public string PesquisarPorCpf(String cpf)
+        {
+            string retorno = $"CPF: {cpf}\r\n " +
+                $"Nome: {nome}" +
+                $"Nome da Mãe: {nomeMae}" +
+                $"Telefone: {telefone}";
+            return retorno;
+
+        }
+    }
+}
---
+++ 10_18/PessoaController.cs
@@ -0,0 +1,49 @@
+﻿using ExemploWebApiPoo.Dominio;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExemploWebApiPoo.Controllers
+{
+    public class PessoaController : ControllerBase
+    {
+        [HttpPost("InserirPessoa")]
+        public string InserirPessoa(string cpfPessoa, string nomePessoa,
+                                    DateTime dataNascimento, string telefone,
+                                    string nomeMaePessoa, string emailPessoa) 
+        {
+            return "Registro inserido com sucesso";
+        }
+        
+        [HttpPut("AlterarPessoa")]
+        public string AlterarPessoa(string cpfPessoa, string nomePessoa,
+                                    DateTime dataNascimento, string telefone,
+                                    string nomeMaePessoa, string emailPessoa) 
+        {
+            return "Registro alterado com sucesso";
+        }
+        
+        [HttpDelete("ExcluirPessoa")]
+        public string ExcluirPessoa(string cpfPessoa, string nomePessoa,
+                                    DateTime dataNascimento, string telefone,
+                                    string nomeMaePessoa, string emailPessoa) 
+        {
+            return "Registro Excluido com sucesso";
+        }
+        
+        [HttpGet("PesquisarTodos")]
+        public string PesquisarTodos(string cpfPessoa, string nomePessoa,
+                                    DateTime dataNascimento, string telefone,
+                                    string nomeMaePessoa, string emailPessoa) 
+        {
+            Pessoa pessoa = new Pessoa();
+
+            pessoa.cpf = cpfPessoa;
+            pessoa.nome = nomePessoa;
+            pessoa.dataNascimento = dataNascimento;
+            pessoa.telefone = telefone;
+            pessoa.nomeMae = nomeMaePessoa;
+            pessoa.email = emailPessoa;
+
+            return pessoa.PesquisarTodos();
+        }
+    }
+}
